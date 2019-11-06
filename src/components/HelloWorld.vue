55820a9517e33321ad2887e2d05bbf2433fee9de
kensaku33 <picow.zoe33@gmail.com>
2019-11-06 17:45:56 +0900

    firebaseと繋げた

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,11 +12,12 @@
     </ul>
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">コッち！</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">こっちだって！</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">あ、すいません</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">石を投げないで！</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">帰る！</a></li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,49 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// font-awesome用追記
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// 以下の３つを使うようにimport
+import {
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// importしたアイコンをライブラリに追加
+library.add(
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+)
+Vue.component('fa', FontAwesomeIcon)
+// ここまでがfont-awesome
+
+
+// ここからはfirebaseの設定
+import { firestorePlugin } from 'vuefire'          
+import firebase from 'firebase'     
+import 'firebase/firestore'
+
+Vue.use(firestorePlugin)
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCNSoL8EO6Ld4E6HbR4OGqCFlDcCvXOqoA",
+  authDomain: "whisperer-d802a.firebaseapp.com",
+  databaseURL: "https://whisperer-d802a.firebaseio.com",
+  projectId: "whisperer-d802a",
+  storageBucket: "whisperer-d802a.appspot.com",
+  messagingSenderId: "769244203241",
+  appId: "1:769244203241:web:c357f66b26d46888ed4878"
+})
+
+export const db = firebase.firestore()
+export const auth = firebase.auth()
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
